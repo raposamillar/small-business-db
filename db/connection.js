cf54926c066fe0846c8c213f3b3f598bdefe52b9
raposamillar <lisa.raposamillar@gmail.com>
2022-10-17 11:39:37 -0400

    create db connection

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:process.env.PW,
+    database:'small_business_db'
+  });
+
+  db.connect();
+
+  module.exports = db;
